9dbe50d6591336d7df62bf8f197addcaa9808160
Atul Bipin <atulbipin@gmail.com>
2018-08-04 09:41:54 -0700

    Add dummy signing spotify function

--- spotify.py
+++ spotify.py
@@ -1,7 +1,13 @@
 import spotipy
+
 sp = spotipy.Spotify()
 
-results = sp.search(q='weezer', limit=20)
+results = sp.search(q="weezer", limit=20)
 
-for i, t in enumerate(results['tracks']['items']):
-    print(' ', i, t['name'])
+for i, t in enumerate(results["tracks"]["items"]):
+    print(" ", i, t["name"])
+
+
+def signin(email, password):
+    # TODO: Add spotify signin logic here
+    pass
